acb0f741698b1392167fb80f0aee65aa2a4c48ce
AnnaMintel <annikamintel99@yandex.ru>
2022-01-27 23:01:18 +0300

    init: created tasks from 8kyu Codewars

---
+++ 8kyu.js
@@ -0,0 +1,18 @@
+// Function 1 - hello world
+// Write a function "greet" that returns "hello world"
+function greet( ){
+  return "hello world!";
+}
+
+
+/* Your task is to create the functionisDivideBy (or is_divide_by) 
+to check if an integer number is divisible by both integers a and b. */
+function isDivideBy(number, a, b) {
+  if (number % a === 0 && number % b === 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
