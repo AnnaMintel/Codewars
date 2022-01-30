1bc5110807994f4c51b4a6b1b35b1544f73d4edc
AnnaMintel <annikamintel99@yandex.ru>
2022-01-30 23:50:51 +0300

    feat: added new tasks

--- 8kyu.js
+++ 8kyu.js
@@ -15,7 +15,22 @@ function isDivideBy(number, a, b) {
   }
 }
 
+/* Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as 
+intended (i.e. make the first character in the string "word" upper case).
+Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will 
+be from 1 character up to 10 characters, but will never be empty.
+*/
+
+
+
+
+
+
 // Given a year, return the century it is in
+
+
+
 
 
 // We need a function that can transform a number into a string
@@ -24,6 +39,31 @@ function numberToString(num) {
   let str = number.toString();
   return str;
 }
+OR
+function numberToString(num) {
+   return num.toString();
+}
+
+// Convert a Number to a String
+
+
+
+
+
+// Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+
+
+
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+
+
+
+
+
 
 
 
