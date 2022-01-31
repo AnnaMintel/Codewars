7d52981822529978de82c62cdfbea9a1a1eab246
AnnaMintel <annikamintel99@yandex.ru>
2022-01-31 23:22:36 +0300

    feat: added new tasks

--- 7kyu.js
+++ 7kyu.js
@@ -1,5 +1,14 @@
-// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// Determine the area of the largest square that can fit inside a circle with radius r
+function areaLargestSquare(r) {
+  return (r * (r*2));
+}
 
+
+/* Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 
+66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows. */
+function digits(n) {
+  return n.toString().length;
+}
 
 
 
--- 8kyu.js
+++ 8kyu.js
@@ -73,7 +73,17 @@ function even_or_odd(number) {
 
 
 // Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
-
+function fakeBin(x){
+  let res = [];
+  for (let i = 0; i < x.length; i++){
+    if (x[i] < 5){
+      res.push(0);
+    } else {
+      res.push(1);
+    }
+  }
+  return res.join('');
+}
 
 
 
