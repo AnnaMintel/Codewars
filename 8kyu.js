9f21394ec09c354c13030c8b46db3cbe3e3cd35d
AnnaMintel <annikamintel99@yandex.ru>
2022-02-04 00:00:25 +0300

    feat: added new task

--- 8kyu.js
+++ 8kyu.js
@@ -159,7 +159,15 @@ switch (number) {
 }
 
 
-//
+// Return the type of the sum of the two arguments
+
+
+
+/*You are given two interior angles (in degrees) of a triangle.
+Write a function to return the 3rd.Note: only positive integers will be tested.*/
+function otherAngle(a, b) {
+  return 180 - (a+b);
+}
 
 
 
