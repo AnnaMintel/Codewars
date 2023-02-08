dab92c68885745b29dea6489b7cf96b57406339f
AnnaMintel <annikamintel99@yandex.ru>
2023-02-08 16:27:04 +0300

    feat: added new task

--- 8kyu.js
+++ 8kyu.js
@@ -231,10 +231,13 @@ function digitize(n) {
 /* Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be 
 negative or non-integer. If the array does not contain any numbers then you should return 0. */
 function sum (numbers) {
-return numbers.reduce((acc, number) => acc + number, 0);
+  return numbers.reduce((acc, number) => acc + number, 0);
 };
 
-
+// convert
+const stringToNumber = function(str){
+  return +str;
+}
 
 
 
